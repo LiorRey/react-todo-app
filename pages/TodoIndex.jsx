01f9c33edefe426c7cd8a42e6bb8d49b9292929b
LiorRey <lior_rey@hotmail.com>
2025-11-14 01:33:47 +0200

    APP DONE !

--- pages/TodoIndex.jsx
+++ pages/TodoIndex.jsx
@@ -12,7 +12,7 @@ import { DataTable } from "../cmps/data-table/DataTable.jsx"
 import { utilService } from "../services/util.service.js"
 import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service.js"
 
-const { useState, useEffect } = React
+const { useEffect } = React
 const { Link, useSearchParams } = ReactRouterDOM
 const { useSelector } = ReactRedux
 
